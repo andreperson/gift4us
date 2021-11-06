6d78e4c5b548c5e86141f0ce0fed92e79d624fa3
44810 <44810@SA3-611892.PMSBC.GOV.BR>
2021-11-06 19:14:44 -0300

    alteracoes deste commit:
    - retirando target

--- target/gift4us/resources/Scripts/jqueryenterdesable.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('input').keypress(function (e) {
-        var code = null;
-        code = (e.keyCode ? e.keyCode : e.which);
-        return (code == 13) ? false : true;
-    });
-});
