4ab0d50effe121feac29cd4ca88b3d0f9c5e381b
SONY <SONY@DESKTOP-CPAOEF3>
2022-05-30 21:08:33 -0300

    alterações deste commit:
    - ajuste na campanha 2

--- src/main/java/br/com/gift4us/site/IndexController.java
+++ src/main/java/br/com/gift4us/site/IndexController.java
@@ -60,16 +60,16 @@ public class IndexController {
 
 	@Autowired
 	private OrcamentoDAO orcamentoDAO;
-	
+
 	@Autowired
 	private AnuncianteDAO anuncianteDAO;
 
 	@Autowired
 	private MensagensDoSistemaDAO mensagensDoSistemaDAO;
-	
+
 	@Autowired
 	private ConfiguracoesDoSistemaDAO configuracoesDAO;
-	
+
 	@Autowired
 	private ProdutoDAO produtoDAO;
 
@@ -97,27 +97,24 @@ public class IndexController {
 		model.addAttribute("listaMenuCategoria", montaMenuCategoria());
 		model.addAttribute("listaMenuCampanhaLinha", buscaLinha(buscaCampanhaPorOrdem(2)));
 		model.addAttribute("listaMenuCampanhaEspecial", buscaLinha(buscaCampanhaPorOrdem(1)));
-		
 
-		
 		return "site/index/index";
 	}
 
 	private List<CategoriaModel> montaMenuCategoria() {
 		List<CategoriaModel> lst = new ArrayList<CategoriaModel>();
 		lst = categoriaDAO.listaTudo();
-		
+
 		return lst;
 	}
 
 	private List<LinhaModel> montaMenuLinha(CampanhaModel campanha) {
 		List<LinhaModel> lst = new ArrayList<LinhaModel>();
 		lst = linhaDAO.buscaPorCampanha(campanha);
-		
+
 		return lst;
 	}
 
-	
 	private void montaCampanha1(Model model) {
 
 		List<ProdutoModel> lstProd = new ArrayList<ProdutoModel>();
@@ -143,18 +140,21 @@ public class IndexController {
 	private void montaCampanha2(Model model) {
 
 		List<ProdutoModel> lstProd = new ArrayList<ProdutoModel>();
-		CampanhaModel campanha = buscaCampanhaPorOrdem(2);
-		model.addAttribute("Campanha2", campanha);
-		List<LinhaModel> lstLinha = new ArrayList<LinhaModel>();
-		if (campanha != null) {
-			if (campanha.getId() != null) {
-				lstLinha = buscaLinha(campanha);
-				LinhaModel linha = escolheLinhaAleatoria(lstLinha);
 
-				model.addAttribute("Linha2", linha);
-				lstProd = new ArrayList<ProdutoModel>();
-				lstProd = buscaProduto(linha);
-				model.addAttribute("ProdutoCampanha2", lstProd);
+		List<LinhaModel> lstLinha = new ArrayList<LinhaModel>();
+		CampanhaModel camp = buscaCampanhaPorOrdem(2);
+		if (camp != null) {
+			if (camp.getId() != null) {
+				model.addAttribute("Campanha2", camp);
+				lstLinha = buscaLinha(camp);
+				if (lstLinha.size() > 0) {
+					LinhaModel linha = escolheLinhaAleatoria(lstLinha);
+					model.addAttribute("Linha2", linha);
+					lstProd = new ArrayList<ProdutoModel>();
+					lstProd = buscaProduto(linha);
+					model.addAttribute("ProdutoCampanha2", lstProd);
+				}
+
 			}
 		}
 	}
@@ -180,6 +180,12 @@ public class IndexController {
 				break;
 			}
 			aleatoria = linha;
+		}
+
+		if (aleatoria == null) {
+			for (LinhaModel linha : lstLinha) {
+				aleatoria = linha;
+			}
 		}
 
 		return aleatoria;
@@ -213,93 +219,88 @@ public class IndexController {
 		Cookie cookie = getCookie(request, "gift4us-cart");
 		List<Cart> lstCart = new ArrayList<Cart>();
 		List<ProdutoModel> lstProdutos = new ArrayList<ProdutoModel>();
-		
 
 		if (cookie != null) {
-			if(cookie.getValue() != ""){
+			if (cookie.getValue() != "") {
 				lstCart = extracaoProdutosDoCookie(cookie.getValue());
 				lstProdutos = produtoDAO.listaProdutosDoCarrinho(extracaoProdutosIds(lstCart));
 				insereQtdes(lstProdutos, lstCart);
 			}
 
 		}
-		
+
 		model.addAttribute("lstProdutos", lstProdutos);
 		model.addAttribute("urlpadrao", propriedades.getValor("arquivo.diretorio.arquivos"));
-		
+
 		return "site/index/cart";
 	}
-	
+
 	@RequestMapping(value = ListaDeURLs.CHECKOUT, method = RequestMethod.GET)
-	public String checkout(@Valid String nome, @Valid String email, @Valid String ddd, @Valid String celular, Model model, HttpServletResponse response, HttpServletRequest request) {
+	public String checkout(@Valid String nome, @Valid String email, @Valid String ddd, @Valid String celular,
+			Model model, HttpServletResponse response, HttpServletRequest request) {
 
 		Cookie cookie = getCookie(request, "gift4us-cart");
 		List<Cart> lstCart = new ArrayList<Cart>();
 		List<ProdutoModel> lstProdutos = new ArrayList<ProdutoModel>();
-		
 
 		if (cookie != null) {
-			if(cookie.getValue() != ""){
+			if (cookie.getValue() != "") {
 				lstCart = extracaoProdutosDoCookie(cookie.getValue());
 				lstProdutos = produtoDAO.listaProdutosDoCarrinho(extracaoProdutosIds(lstCart));
 				insereQtdes(lstProdutos, lstCart);
 			}
 
 		}
-		
+
 		OrcamentoModel orcamento = new OrcamentoModel();
 		model.addAttribute("orcamento", orcamento);
 		model.addAttribute("lstProdutos", lstProdutos);
 		model.addAttribute("urlpadrao", propriedades.getValor("arquivo.diretorio.arquivos"));
-		
+
 		return "site/index/checkout";
 	}
-	
+
 	@RequestMapping(value = ListaDeURLs.CHECKOUT, method = RequestMethod.POST)
-	public String checkout(@Valid OrcamentoModel orcamento, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes, HttpServletResponse response, HttpServletRequest request) {
+	public String checkout(@Valid OrcamentoModel orcamento, BindingResult bindingResult, Model model,
+			RedirectAttributes redirectAttributes, HttpServletResponse response, HttpServletRequest request) {
 
 		Cookie cookie = getCookie(request, "gift4us-cart");
 		List<Cart> lstCart = new ArrayList<Cart>();
 		List<ProdutoModel> lstProdutos = new ArrayList<ProdutoModel>();
 
 		if (cookie != null) {
-			if(cookie.getValue() != ""){
+			if (cookie.getValue() != "") {
 				lstCart = extracaoProdutosDoCookie(cookie.getValue());
 				lstProdutos = produtoDAO.listaProdutosDoCarrinho(extracaoProdutosIds(lstCart));
 				insereQtdes(lstProdutos, lstCart);
 			}
 		}
-		
+
 		gravaOrcamento(lstProdutos, orcamento);
-		
-		enviaEmailOrcamento(orcamento, lstCart, lstProdutos);	
-		
+
+		enviaEmailOrcamento(orcamento, lstCart, lstProdutos);
+
 		enviaEmailOrcamentoCliente(orcamento, lstProdutos);
-		
-		//limpa o carrinho
+
+		// limpa o carrinho
 		cookie.setMaxAge(-1);
-		
+
 		model.addAttribute("lstProdutos", lstProdutos);
 		model.addAttribute("urlpadrao", propriedades.getValor("arquivo.diretorio.arquivos"));
-		
+
 		return "site/index/checkout-limpa";
 	}
-	
-	
+
 	@RequestMapping(value = ListaDeURLs.CHECKOUT_LIMPA, method = RequestMethod.GET)
-	public String checkoutlimpa(BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes, HttpServletResponse response, HttpServletRequest request) {
+	public String checkoutlimpa(BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes,
+			HttpServletResponse response, HttpServletRequest request) {
 
-
-		
-		
 		return "site/index/checkout-limpa";
 	}
-	
-	
-	
+
 	private void gravaOrcamento(List<ProdutoModel> lstProdutos, OrcamentoModel orcamento) {
 		OrcamentoModel orcaSalva = new OrcamentoModel();
-		
+
 		for (ProdutoModel prd : lstProdutos) {
 			orcaSalva = new OrcamentoModel();
 			orcaSalva.setAnunciante(prd.getAnunciante());
@@ -312,10 +313,9 @@ public class IndexController {
 			orcaSalva.setProduto(prd);
 			orcaSalva.setQuantidade(prd.getQtdademin());
 			orcaSalva.setStatus(OrcamentoEnum.NOVO.getValor());
-			orcamentoDAO.insere(orcaSalva);	
+			orcamentoDAO.insere(orcaSalva);
 		}
 	}
-	
 
 	private List<ProdutoModel> insereQtdes(List<ProdutoModel> lstProdutos, List<Cart> lstCart) {
 
@@ -347,17 +347,17 @@ public class IndexController {
 		Set<AnuncianteModel> set = new HashSet<AnuncianteModel>();
 		AnuncianteModel anunciante = new AnuncianteModel();
 
-		for (ProdutoModel prd: lst) {
+		for (ProdutoModel prd : lst) {
 			anunciante = new AnuncianteModel();
 			anunciante = prd.getAnunciante();
 			set.add(anunciante);
 		}
-		
+
 		lstAnunciante.addAll(set);
 
 		return lstAnunciante;
 	}
-	
+
 	private List<Cart> extracaoProdutosDoCookie(String cookie) {
 
 		String[] arrayCookie = cookie.split("-");
@@ -391,164 +391,165 @@ public class IndexController {
 	}
 
 	private Boolean enviaEmailOrcamentoCliente(OrcamentoModel orcamento, List<ProdutoModel> lstProdutos) {
-		boolean enviaPara=false;
-		
+		boolean enviaPara = false;
+
 		String ambiente = System.getenv("AMBIENTE");
 
 		String destinatario = orcamento.getEmail();
 		String assunto = "Orcamento Solicitado | gift4us";
-		
+
 		Propriedades util = new Propriedades();
 		MailConfig config = new MailConfig();
 		Properties props = new Properties();
 		config = config.setConfigeProperties(configuracoesDAO);
 		props = util.setProps(config);
-		
+
 		String emailfrom = config.getEmailfrom();
 		String emailsenha = config.getSenha();
-		
+
 		StringBuilder corpo = new StringBuilder();
-		
-		corpo.append(pegaCabecalho(orcamento).append(pegaInicio().append(pegaCorpo(lstProdutos, true).append(pegaRodape()))));
+
+		corpo.append(pegaCabecalho(orcamento)
+				.append(pegaInicio().append(pegaCorpo(lstProdutos, true).append(pegaRodape()))));
 
 		if (ambiente.equals("producao")) {
 			enviaPara = Mail.EnviarEmail(props, emailfrom, emailsenha, corpo.toString(), assunto, destinatario, null,
-					null);	
-		}
-		else {
+					null);
+		} else {
 			System.out.println("email" + corpo);
 		}
 
 		return enviaPara;
 	}
-	
-	
+
 	private Boolean enviaEmailOrcamento(OrcamentoModel orcamento, List<Cart> lstCart, List<ProdutoModel> lstProdutos) {
-		boolean enviaPara=false;
-		
+		boolean enviaPara = false;
+
 		String ambiente = System.getenv("AMBIENTE");
 
 		String destinatario = "andrep.person@gmail.com";
 		String assunto = "Orcamento | gift4us";
-		
+
 		Propriedades util = new Propriedades();
 		MailConfig config = new MailConfig();
 		Properties props = new Properties();
 		config = config.setConfigeProperties(configuracoesDAO);
 		props = util.setProps(config);
-		
+
 		String emailfrom = config.getEmailfrom();
 		String emailsenha = config.getSenha();
 
-		
 		List<ProdutoModel> lstPrdAnunciante = new ArrayList<ProdutoModel>();
 		List<AnuncianteModel> lstAnunciante = new ArrayList<AnuncianteModel>();
 		lstAnunciante = extracaoAnuncianteIds(lstProdutos);
-		
+
 		StringBuilder corpo = new StringBuilder();
-		
-		
+
 		for (AnuncianteModel anu : lstAnunciante) {
 			lstPrdAnunciante = new ArrayList<ProdutoModel>();
 			lstPrdAnunciante = produtoDAO.listaProdutosDoCarrinhoPorAnunciante(extracaoProdutosIds(lstCart), anu);
-			
-				corpo.append(pegaCabecalho(orcamento).append(pegaInicio().append(pegaCorpo(lstPrdAnunciante, false).append(pegaRodape()))));
-				
-				if (ambiente.equals("producao")) {
-					enviaPara = Mail.EnviarEmail(props, emailfrom, emailsenha, corpo.toString(), assunto, destinatario, null,
-							null);	
-				}
-				else {
-					System.out.println("email" + corpo);
-				}
+
+			corpo.append(pegaCabecalho(orcamento)
+					.append(pegaInicio().append(pegaCorpo(lstPrdAnunciante, false).append(pegaRodape()))));
+
+			if (ambiente.equals("producao")) {
+				enviaPara = Mail.EnviarEmail(props, emailfrom, emailsenha, corpo.toString(), assunto, destinatario,
+						null, null);
+			} else {
+				System.out.println("email" + corpo);
+			}
 		}
 		return enviaPara;
 	}
-	
+
 	private StringBuilder pegaCabecalho(OrcamentoModel orcamento) {
 		StringBuilder texto = new StringBuilder();
-		texto.append("<!DOCTYPE html> <html> <head> <title>Orcamento gift4us</title> <style> html, body { min-height: 100%; } body, div, form, input, p { padding: 0; margin: 0; outline: none; font-family: Roboto, Arial, sans-serif; font-size: 14px; color: #666; line-height: 22px; } h1 { font-weight: 400; } .testbox { display: flex; justify-content: center; align-items: center; height: inherit; padding: 3px; } form { width: 100%; padding: 20px; background: #fff; box-shadow: 0 2px 5px #ccc; } input { width: calc(100% - 10px); padding: 5px; border: 1px solid #ccc; border-radius: 3px; vertical-align: middle; } input:hover, textarea:hover { outline: none; border: 1px solid #095484; } th, td { width: 28%; padding: 15px 0; border-bottom: 1px solid #ccc; text-align: center; vertical-align: unset; line-height: 18px; font-weight: 400; word-break: break-all; } .first-col { width: 16%; text-align: left; } textarea:hover { outline: none; border: 1px solid #1c87c9; } table { width: 100%; } textarea { width: calc(100% - 6px); } .question { padding: 15px 0 5px; color: #095484; } .question-answer label { display: block; padding: 0 20px 10px 0; } .question-answer input { width: auto; } .btn-block { margin-top: 20px; text-align: center; } button { width: 150px; padding: 10px; border: none; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; background-color: #095484; font-size: 16px; color: #fff; cursor: pointer; } button:hover { background-color: #0666a3; } @media (min-width: 568px) { th, td { word-break: keep-all; } } </style> </head> <body> <div class='testbox'> <form action='/'> <h1>Orçamento gift4us</h1> <p class='question'>");
+		texto.append(
+				"<!DOCTYPE html> <html> <head> <title>Orcamento gift4us</title> <style> html, body { min-height: 100%; } body, div, form, input, p { padding: 0; margin: 0; outline: none; font-family: Roboto, Arial, sans-serif; font-size: 14px; color: #666; line-height: 22px; } h1 { font-weight: 400; } .testbox { display: flex; justify-content: center; align-items: center; height: inherit; padding: 3px; } form { width: 100%; padding: 20px; background: #fff; box-shadow: 0 2px 5px #ccc; } input { width: calc(100% - 10px); padding: 5px; border: 1px solid #ccc; border-radius: 3px; vertical-align: middle; } input:hover, textarea:hover { outline: none; border: 1px solid #095484; } th, td { width: 28%; padding: 15px 0; border-bottom: 1px solid #ccc; text-align: center; vertical-align: unset; line-height: 18px; font-weight: 400; word-break: break-all; } .first-col { width: 16%; text-align: left; } textarea:hover { outline: none; border: 1px solid #1c87c9; } table { width: 100%; } textarea { width: calc(100% - 6px); } .question { padding: 15px 0 5px; color: #095484; } .question-answer label { display: block; padding: 0 20px 10px 0; } .question-answer input { width: auto; } .btn-block { margin-top: 20px; text-align: center; } button { width: 150px; padding: 10px; border: none; -webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; background-color: #095484; font-size: 16px; color: #fff; cursor: pointer; } button:hover { background-color: #0666a3; } @media (min-width: 568px) { th, td { word-break: keep-all; } } </style> </head> <body> <div class='testbox'> <form action='/'> <h1>Orçamento gift4us</h1> <p class='question'>");
 		texto.append("Solicitação de preços<br><br> ");
-		texto.append("Cliente <br>" + orcamento.getNome() + " <br> " + orcamento.getEmail() + "<br>" + orcamento.getDdd() + " " + orcamento.getCelular() + "</p>");
+		texto.append("Cliente <br>" + orcamento.getNome() + " <br> " + orcamento.getEmail() + "<br>"
+				+ orcamento.getDdd() + " " + orcamento.getCelular() + "</p>");
 		return texto;
 	}
-
 
 	private StringBuilder pegaInicio() {
 		StringBuilder texto = new StringBuilder();
 		texto.append("<br><br>");
-		texto.append("<table class='table align-items-center table-flush'> <thead class='thead-light'> <tr style='background:#999; color:#fff'> <th scope='col'>Produto</th> <th scope='col'>Preço</th> <th scope='col'>Quantidade</th> <th scope='col'>Total</th> <th scope='col'></th> </tr> </thead>");
+		texto.append(
+				"<table class='table align-items-center table-flush'> <thead class='thead-light'> <tr style='background:#999; color:#fff'> <th scope='col'>Produto</th> <th scope='col'>Preço</th> <th scope='col'>Quantidade</th> <th scope='col'>Total</th> <th scope='col'></th> </tr> </thead>");
 		return texto;
 	}
-	
-	
+
 	private StringBuilder pegaCorpo(List<ProdutoModel> lstProdutos, Boolean cliente) {
 		Double precocomdesconto = null;
-		Double precoxqtde= (double) 0;
-		Double totalgeral= (double) 0;
+		Double precoxqtde = (double) 0;
+		Double totalgeral = (double) 0;
 		StringBuilder texto = new StringBuilder();
 		texto.append("<tbody>");
-		
+
 		String urlprodutomontada = "";
 		String urlpadrao = propriedades.getValor("arquivo.diretorio.arquivos");
 		String urlmodelo = urlpadrao + "/_modelos/";
 		String urlproduto = urlpadrao + "/_produtos/";
-		
+
 		for (ProdutoModel prd : lstProdutos) {
 			urlprodutomontada = urlmodelo + "modelo-220x330.jpg";
-			
-			if(prd.getImagem() != null) {
-				urlprodutomontada = urlproduto + "/" + prd.getAnunciante().getId() + "/" + prd.getId() + "/" + prd.getImagem();
+
+			if (prd.getImagem() != null) {
+				urlprodutomontada = urlproduto + "/" + prd.getAnunciante().getId() + "/" + prd.getId() + "/"
+						+ prd.getImagem();
 			}
-			
-			if(prd.getDesconto() != null) {
+
+			if (prd.getDesconto() != null) {
 				precocomdesconto = prd.getPreco() - (prd.getPreco() * prd.getDesconto());
-			}
-			else {
+			} else {
 				precocomdesconto = prd.getPreco();
 			}
-			
-			precoxqtde = prd.getQtdademin() * precocomdesconto;
-			totalgeral += precoxqtde; 
 
-			texto.append("<tr style='background:#f7f7f7'><td class='first-col'><a href='https://gift4us.com.br/site/produtos/produto/'" + prd.getId() + "'>");
-			texto.append("<img src='" + urlprodutomontada + "' alt='' width='40px;' class='img-thumbnail' /></a> &nbsp;&nbsp;&nbsp;");
-			if(cliente) {
-				texto.append("<small>" + prd.getTitulo() + " - de: " + prd.getAnunciante().getFantasia() + "</small></td>");
-			}
-			else {
-				texto.append("<small>" + prd.getTitulo() + "</small></td>");	
+			precoxqtde = prd.getQtdademin() * precocomdesconto;
+			totalgeral += precoxqtde;
+
+			texto.append(
+					"<tr style='background:#f7f7f7'><td class='first-col'><a href='https://gift4us.com.br/site/produtos/produto/'"
+							+ prd.getId() + "'>");
+			texto.append("<img src='" + urlprodutomontada
+					+ "' alt='' width='40px;' class='img-thumbnail' /></a> &nbsp;&nbsp;&nbsp;");
+			if (cliente) {
+				texto.append(
+						"<small>" + prd.getTitulo() + " - de: " + prd.getAnunciante().getFantasia() + "</small></td>");
+			} else {
+				texto.append("<small>" + prd.getTitulo() + "</small></td>");
 			}
 			texto.append("<td>" + precocomdesconto + "</td>");
 			texto.append("<td>" + prd.getQtdademin() + "</td>");
 			texto.append("<td>" + precoxqtde + "</td></tr>");
 		}
-		
-	texto.append("<tr style='background:#f7f7f7; color:blue'><td>&nbsp;</td>");
+
+		texto.append("<tr style='background:#f7f7f7; color:blue'><td>&nbsp;</td>");
 		texto.append("<td></td>");
 		texto.append("<td>Total: </td>");
 		texto.append("<td>" + totalgeral + "</td></tr>");
 		texto.append("</tbody></table>");
-		
+
 		return texto;
-		
+
 	}
-	
+
 	private StringBuilder pegaRodape() {
-		
+
 		StringBuilder texto = new StringBuilder();
 		texto.append(" <br><br><div class='card-footer py-4'> <nav aria-label='...'> </nav> </div> ");
-		texto.append("<footer class='footer'> <div class='row align-items-center justify-content-xl-between'> <div class='col-xl-6'> <hr>" + pegaDataAtual() + "</div>  </div> </footer>  </body>  </html>");
+		texto.append(
+				"<footer class='footer'> <div class='row align-items-center justify-content-xl-between'> <div class='col-xl-6'> <hr>"
+						+ pegaDataAtual() + "</div>  </div> </footer>  </body>  </html>");
 		return texto;
 	}
-	
-	
+
 	private String pegaDataAtual() {
 		DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 		Date date = new Date();
 		return dateFormat.format(date).toString();
-		
+
 	}
-	
 
 }
